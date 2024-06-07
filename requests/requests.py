003b7dd2d7c3e97c5a601bfdb76c13a78619cb43
Olga_W <giredo92@yandex.ru>
2024-06-07 15:26:28 +0300

    5|7

---
+++ requests/requests.py
@@ -0,0 +1,33 @@
+{
+    "first_name": "Roma",
+    "last_name": "Kopytov",
+}
+
+{
+    "name": "Sisya",
+    "color": "black",
+    "birth_year": 2021,
+    "owner": 1
+}
+
+{
+    "name": "Барсик",
+    "color": "#000000",
+    "birth_year": 2017,
+    "achievements": [
+        {"achievement_name": "поймал мышку"},
+        {"achievement_name": "разбил вазу"}
+    ]
+}
+
+{
+    "username": "Author",
+    "password": "9293oderiG)"
+}
+"auth_token": "3636ba4a0fd3aa18caee1d766a44d08200e6e69a"
+{
+    "username": "Reader",
+    "password": "9293oderiG)"
+}
+
+"auth_token": "f3b351c04c98845bd1be72d98e69d4b57097979d"
